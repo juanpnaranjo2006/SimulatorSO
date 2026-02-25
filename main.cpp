45832ed4dede9dba3d1e2299442a9698c40e1ef5
juanpnaranjo2006 <juanpnaranjo2006@javerianacali.edu.co>
2026-02-25 16:47:26 -0500

    NEW: Decode para instrucciones que recibe el procesador

--- main.cpp
+++ main.cpp
@@ -7,7 +7,43 @@
 std::vector <std::string> memoria(1024);
 std::map <std::string, int> decodificador;
 
-int decodificar(std::string &strAddr, ) {
+int decodificar(std::string &strAddr) {
+    return decodificador[strAddr];
+}
+
+void decode(std::string &instruction) {
+    int opCode = -1;
+    std::string instructType = instruction.substr(0, instruction.find(" "));
+
+    if (instructType == "SET") {
+        opCode = 0;
+    }
+    else if (instructType == "LDR") {
+        opCode = 1;
+    }
+    else if (instructType == "ADD") {
+        opCode = 2;
+    }
+    else if (instructType == "INC") {
+        opCode = 3;
+    }
+    else if (instructType == "DEC") {
+        opCode = 4;
+    }
+    else if (instructType == "STR") {
+        opCode = 5;
+    }
+    else if (instructType == "SHW") {
+        opCode = 6;
+    }
+    else if (instructType == "PAUSE") {
+        opCode = 7;
+    }
+    else if (instructType == "END") {
+        opCode = 8;
+    }
+    
+    return opCode;
 }
 
 
