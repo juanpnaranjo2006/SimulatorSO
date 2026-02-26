088f55d5d08e52646ce146fdd6e26664a6118c47
juanpnaranjo2006 <juanpnaranjo2006@javerianacali.edu.co>
2026-02-26 07:59:07 -0500

    NEW: Cambios en la clase CPU e implementaciones de todos sus métodos

--- cpu.cpp
+++ cpu.cpp
@@ -3,3 +3,99 @@
 //
 
 #include "cpu.h"
+#include <sstream>
+
+void CPU::decode(const std::string &instructName) {
+    opCode = -1;
+
+    if (instructName == "SET") {
+        opCode = 0;
+    }
+    else if (instructName == "LDR") {
+        opCode = 1;
+    }
+    else if (instructName == "ADD") {
+        opCode = 2;
+    }
+    else if (instructName == "INC") {
+        opCode = 3;
+    }
+    else if (instructName == "DEC") {
+        opCode = 4;
+    }
+    else if (instructName == "STR") {
+        opCode = 5;
+    }
+    else if (instructName == "SHW") {
+        opCode = 6;
+    }
+    else if (instructName == "PAUSE") {
+        opCode = 7;
+    }
+    else if (instructName == "END") {
+        opCode = 8;
+    }
+}
+
+void CPU::setArguments() {
+    //Manipula el ICR con substrings para poder tener los argumentos de la instrucción separados
+    std::istringstream iss(icr);
+    std::string instructName;
+    iss >> instructName;
+    decode(instructName);
+
+    for (int i = 0; i < 3; ++i) {
+        arguments[i] = "";
+        iss >> arguments[i];
+    }
+}
+
+// ===== Getters =====
+int CPU::getMAR() const {
+    return mar;
+}
+
+int CPU::getMDR() const {
+    return mdr;
+}
+
+std::string CPU::getICR() const {
+    return icr;
+}
+
+int CPU::getProgramCounter() const {
+    return programCounter;
+}
+
+std::vector<std::string> CPU::getArguments() const {
+    return arguments;
+}
+
+int CPU::getOpCode() const {
+    return opCode;
+}
+
+// ===== Setters =====
+void CPU::setMAR(int value) {
+    mar = value;
+}
+
+void CPU::setMDR(int value) {
+    mdr = value;
+}
+
+void CPU::setICR(const std::string& value) {
+    icr = value;
+}
+
+void CPU::setProgramCounter(int value) {
+    programCounter = value;
+}
+
+void CPU::setArguments(const std::vector<std::string>& args) {
+    arguments = args;
+}
+
+void CPU::setOpCode(int value) {
+    opCode = value;
+}
--- cpu.h
+++ cpu.h
@@ -5,6 +5,7 @@
 #ifndef SIMULATORSO_CPU_H
 #define SIMULATORSO_CPU_H
 #include <string>
+#include <vector>
 
 
 class CPU {
@@ -14,9 +15,19 @@ private:
     std::string icr;
     int programCounter;
 
+    // ===== Variables Auxiliares =====
+    std::vector <std::string> arguments{3};
+    int opCode;
+
+    // ===== Métodos privados =====
+    void decode(const std::string &instructName);
+    void setArguments();
+
 public:
     CPU() = default;
-    int decode();
+
+    // ===== Métodos principales =====
+
 
 
     // ===== Getters =====
@@ -24,12 +35,16 @@ public:
     int getMDR() const;
     std::string getICR() const;
     int getProgramCounter() const;
+    std::vector<std::string> getArguments() const;
+    int getOpCode() const;
 
     // ===== Setters =====
     void setMAR(int value);
     void setMDR(int value);
     void setICR(const std::string& value);
     void setProgramCounter(int value);
+    void setArguments(const std::vector<std::string>& args);
+    void setOpCode(int value);
 };
 
 
