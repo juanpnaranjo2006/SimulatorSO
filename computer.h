500b9161fd2a0d4b4e969d57e1afd511a3f9235f
zulu <cesarzulu82@gmail.com>
2026-03-01 15:49:50 -0500

    Se creo la clase computador y se resolvio el issue 3

---
+++ computer.h
@@ -0,0 +1,23 @@
+#ifndef SIMULATORSO_COMPUTER_H
+#define SIMULATORSO_COMPUTER_H
+#include "cpu.h"
+#include "memory.h"
+
+
+class COMPUTER {
+private:
+    CPU processor;
+    MEMORY memory;
+public:
+    COMPUTER();
+    COMPUTER(int memSize);
+    COMPUTER(int memSize, int textIndex);
+    ~COMPUTER() = default;
+
+    // == Getters ==
+    CPU& getProcessor();
+    MEMORY& getMemory();
+};
+
+
+#endif //SIMULATORSO_COMPUTER_H
